2124049ca7903f343191b3233db4a61fe9103bf9
Max <podmax25@gmail.com>
2022-04-18 21:17:47 +0300

    Нужно ещё немного тестирования

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,13 @@
 from config import BOT_TOKEN_TG
 
+import sqlite3
 import logging
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram.ext import Updater, CommandHandler
+from datetime import *
 
-reply_keyboard = [['/address', '/phone'],
-                  ['/site', '/work_time']]
+reply_keyboard = [['/help', '/add', '/change'],
+                  ['/today', '/day', '/delete']]
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
@@ -35,48 +37,90 @@ def close_keyboard(update, context):
 
 def help(update, context):
     update.message.reply_text(
-        "/add - добавить событее"
-        "/today - посмотреть рассписание на сегодня"
-        "/day - посмотреть рассписание на date день"
-        "/delete - удалить событее"
-        "/change - изменить событее")
+        "/add - Напишите событее в форме 'Название события; год.месяц.день; приложение для отправки', чтобы добавить событее\n"
+        "/today - посмотреть рассписание на сегодня\n"
+        "/day - Введите дату в формате год.месяц.день, чтобы увидеть рассписание на день\n"
+        "/delete - Введите дату и название события в формате 'Название события, год.месяц.день', чтобы удалить событее\n"
+        "/change - изменить событее\n")
 
 
 def add(update, context):
-    update.message.reply_text(
-        "Напишите событее в форме 'Название события; год.месяц.день.время; дополнительные сведенья'")
+    thing = update.message.textlstrip('/add').strip()
+    con = sqlite3.connect("data/things.db")
+    cur = con.cursor()
+    cur.execute(
+        """INSERT INTO tasks_user (username, tasks, date, app) VALUES (?, ?, ?, ?)""",
+        (thing[0], thing[1], thing[2], thing[3]))
+    con.commit()
+    con.close()
+
 
 
 def today(update, context):
-    update.message.reply_text("Рассписание на сегодня:")
+    update.message.reply_text(f"Рассписание на сегодня:")
+    con = sqlite3.connect("data/things.db")
+    cur = con.cursor()
+    today_date = str((datetime.now().date()).strftime("%Y.%m.%d"))
+    tasks = cur.execute(f"""SELECT tasks FROM tasks_user WHERE date='{today_date}'""").fetchall()
+    for task0 in tasks:
+        for task in task0:
+            task = task.split('; ')[0]
+            update.message.reply_text(task)
+    con.commit()
+    con.close()
 
 
 def day(update, context):
-    update.message.reply_text(
-        "Введите дату в формате год.месяц.день, чтобы увидеть рассписание на день")
+    con = sqlite3.connect("data/things.db")
+    cur = con.cursor()
+    need_date = update.message.textlstrip('/day').strip()
+    tasks = cur.execute(
+        f"""SELECT tasks FROM tasks_user WHERE date='{need_date}'""").fetchall()
+    for task0 in tasks:
+        for task in task0:
+            task = task.split('; ')[0]
+            update.message.reply_text(task)
+    con.commit()
+    con.close()
 
 
 def delete(update, context):
-    update.message.reply_text(
-        "Введите дату и название события в формате 'Название события, год.месяц.день', чтобы удалить событее")
+    con = sqlite3.connect("data/things.db")
+    cur = con.cursor()
+    need_task_and_date = str(update.message.text.lstrip('/delete').strip()).split(', ')
+    task, date = need_task_and_date[0], need_task_and_date[1]
+    cur.execute(
+        f"""DELETE from tasks_user where date='{date}' AND tasks='{task}'""").fetchall()
+    update.message.reply_text('Событие удалено')
+    tasks = cur.execute(
+        f"""SELECT tasks FROM tasks_user WHERE date='{date}'""").fetchall()
+    update.message.reply_text('Теперь ваши планы на день:')
+    for task0 in tasks:
+        for task in task0:
+            task = task.split ('; ') [0]
+            update.message.reply_text(task)
+    con.commit()
+    con.close()
 
 
-def change(update, context):
-    update.message.reply_text(
-        "Введите дату и название события в формате 'Название события, год.месяц.день' и новое событее в формате"
-        "'Название события; год.месяц.день.время; дополнительные сведенья' для изменения")
+# def change(update, context):
+#     update.message.reply_text(
+#         "Введите дату и название события в формате 'Название события, год.месяц.день' и новое событее в формате"
+#         " 'Название события; год.месяц.день' для изменения")
+#     thing = yield context.split(';')
 
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
+    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("close", close_keyboard))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("add", add))
     dp.add_handler(CommandHandler("today", today))
     dp.add_handler(CommandHandler("day", day))
     dp.add_handler(CommandHandler("delete", delete))
-    dp.add_handler(CommandHandler("change", change))
+    # dp.add_handler(CommandHandler("change", change))
     updater.start_polling()
 
     updater.idle()
