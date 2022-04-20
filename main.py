44caa720d70a7407eb8310ab13a56f332f8293af
VarvaraEzerskaya <94551739+VarvaraEzerskaya@users.noreply.github.com>
2022-04-20 22:16:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+
 import time
 import sqlite3
 import tg_bot
 import ds_b
 import telegram
 import discord
+from discord.ext import commands
 import config
 
+
+bot = commands.Bot(command_prefix='>')
 TOKEN_TG = config.BOT_TOKEN_TG
 TOKEN_DS = config.BOT_TOKEN_DS
 
-ds_b.run()
+ds_b.bot.run(TOKEN_DS)
 tg_bot.main()
