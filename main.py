c9a1884f0bad9d4a9f5acd00cd2248e4653692a5
psixonaut <94551719+psixonaut@users.noreply.github.com>
2022-04-20 23:21:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-
 import time
 import sqlite3
 import tg_bot
 import ds_b
 import telegram
 import discord
-from discord.ext import commands
 import config
 
-
-bot = commands.Bot(command_prefix='>')
 TOKEN_TG = config.BOT_TOKEN_TG
 TOKEN_DS = config.BOT_TOKEN_DS
 
-ds_b.bot.run(TOKEN_DS)
-tg_bot.main()
+ds_b.run()
+tg_bot.start_work()
