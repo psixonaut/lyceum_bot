d1a32ec62743fdc4bc014b55e15bbd9da6c026f4
psixonaut <94551719+psixonaut@users.noreply.github.com>
2022-04-21 23:05:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-import time
-import sqlite3
-import tg_bot
+from tg_bot import *
 import ds_b
-import telegram
-import discord
 import config
+
 
 TOKEN_TG = config.BOT_TOKEN_TG
 TOKEN_DS = config.BOT_TOKEN_DS
 
-ds_b.run()
-tg_bot.start_work()
+
+def main():
+    updater = Updater(TOKEN_TG)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("add", add))
+    dp.add_handler(CommandHandler("today", today))
+    dp.add_handler(CommandHandler("day", day))
+    dp.add_handler(CommandHandler("delete", delete))
+    # dp.add_handler(CommandHandler("change", change))
+    updater.start_polling()
+    ds_b.bot.run(TOKEN_DS)
+    updater.idle()
+
+
+
+
+if __name__ == '__main__':
+    main()
