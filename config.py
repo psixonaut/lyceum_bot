f2de49f40b6b541d846599347f5b3287d5df4193
psixonaut <94551719+psixonaut@users.noreply.github.com>
2022-04-29 14:26:36 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-vde_ycvjm_nv = '5389484482:lfswxuw7-vj4-cw8uwdpkibuey-uz0axnmv'
-gnn_izpsy_ir = 'iijecenwhob4shvbmxnhthf4.dkxxqv.ea9ndodcb9onxq03fiuowhstcgl'
-gce_ynety_ay = '6983p672fawpo7301i719931sqge0y2144tqj94q0474m5hz56y95656677234q57qfoo945i10z61146wsn3'
-lfzzo_cs = '212033635'
-
-#зашифровано шифром вижнера с квадратом R0T1, алфавит - английский, ключь находится в комментариях к остальному коду
